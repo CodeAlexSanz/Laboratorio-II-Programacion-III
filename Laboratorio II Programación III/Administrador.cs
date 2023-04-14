38bc66e51127d59b3a7f7c8bd563fd8327d2af98
DevAlexSanz <116242323+CodeAlexSanz@users.noreply.github.com>
2023-04-14 12:21:36 -0600

    commit

---
+++ Laboratorio II Programación III/Administrador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_II_Programación_III
+{
+    public class Administrador
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public Administrador(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public Empleado CrearEmpleado(string nombre, string username, string password)
+        {
+            return new Empleado(nombre, username, password);
+        }
+    }
+}
---
+++ Laboratorio II Programación III/Categoría.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_II_Programación_III
+{
+    public class Categoria
+    {
+        public string Nombre { get; set; }
+
+        public Categoria(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+}
